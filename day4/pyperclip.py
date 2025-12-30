dc98f9affc0ccc7ec07c86a69a638b8ad19754b3
GURU-2006-PRO <gurusawarkar786@gmail.com>
2025-12-30 21:45:15 +0530

    issue pyperclip

---
+++ day4/pyperclip.py
@@ -0,0 +1,6 @@
+import pyperclip  ## i facing issue that in my pc there is not pyperclip then i install it 
+pyperclip.copy('Hello Guru')
+pyperclip.paste()
+
+
+## i download it but still facing issues to run i work later
