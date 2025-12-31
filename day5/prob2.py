22529c048014b4e21c1108c85b2c2fb34692c25f
GURU-2006-PRO <gurusawarkar786@gmail.com>
2025-12-31 20:11:04 +0530

    practice the core concept questions on hackerrank

---
+++ day5/prob2.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+print('The sum of Two number is : ',a+b)
+print('The difference of Two number is : ',a-b)
+print('The product of Two number is : ',a*b)
